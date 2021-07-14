705ba2416af060ff49beb3f5ed75b5066e1e309e
Michelle Nguyen <michellenguyen11239@aol.com>
2021-07-14 11:53:44 -0700

    Initial commit

---
+++ controllers/index.js
@@ -0,0 +1,8 @@
+// Import express router
+const express = require('express')
+const router = express.Router()
+
+// Import routes
+
+module.exports = router;
+
---
+++ server.js
@@ -0,0 +1,29 @@
+// Import dependencies 
+const express = require('express');
+const mongoose = require('mongoose');
+const logger = require("morgan");
+
+// Import routes
+const routes = require('./controllers')
+
+// Sets up express 
+const PORT = process.env.PORT || 3000;
+const app = express()
+
+// Sets up morgan logs
+app.use(logger("dev"));
+
+// Sets up data parsing 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Serve static files and use routes
+app.use(express.static("public"));
+app.use(routes)
+
+// Connects to mongoose database
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
+})
